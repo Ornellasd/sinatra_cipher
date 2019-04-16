0066bcda13c2b18534523c383aff8e35db8cbad6
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2019-04-15 20:17:01 -0700

    refactors code, gets rid of unused decryption conditional

--- cipher.rb
+++ cipher.rb
@@ -1,32 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-def cipherize(operation, string, number)
+def cipherize(string, number)
   output = ""
-  if operation == "encrypt" || operation == "e"
-    string.split("").each do |letter|
-      if letter.ord == 32
-        output += 32.chr
-      elsif letter.ord + number > 122
-        output += (letter.ord + number - 26).chr
-      elsif letter.ord + number < 97
-        output += (letter.ord + number + 27).chr
-      else
-        output += (letter.ord + number).chr
-      end  
-    end
-  elsif operation == "decrypt" || operation == "d"
-    string.split("").each do |letter|
-      if letter.ord == 32
-        output += 32.chr
-      elsif letter.ord - number > 122
-        output += (letter.ord - number - 26).chr
-      elsif letter.ord - number < 97
-        output += (letter.ord - number + 26).chr
-      else
-        output += (letter.ord - number).chr
-      end
-    end
+  string.split("").each do |letter|
+    if letter.ord == 32
+      output += 32.chr
+    elsif letter.ord + number > 122
+      output += (letter.ord + number - 26).chr
+    elsif letter.ord + number < 97
+      output += (letter.ord + number + 27).chr
+    else
+      output += (letter.ord + number).chr
+    end  
   end
   output 
 end
@@ -38,5 +24,6 @@ end
 get '/output' do
   text = params["cipher-text"]
   shift = params["shift"]
-  erb :output, :locals => { :output => cipherize('e', text, shift.to_i) }
+  binding.pry
+  erb :output, :locals => { :output => cipherize(text, shift.to_i) }
 end
